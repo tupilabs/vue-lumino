6005f3342e38ffc74147a9db8747aa1c21775ae6
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-06-22 00:34:42 +1200

    Simplify more the component for a generic use case

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,43 @@
 <template>
-  <Lumino />
+  <div>
+    <button @click="buttonClicked">Add</button>
+    <Lumino ref="lumino" />
+  </div>
 </template>
 
 <script>
 import Lumino from '@/components/Lumino'
+import HelloWorld from '@/components/HelloWorld'
 
 export default {
   name: 'Home',
+
   components: {
-    Lumino
+    Lumino,
+    // eslint-disable-next-line vue/no-unused-components
+    HelloWorld
+  },
+
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.$nextTick(() => {
+        vm.$refs.lumino.addWidget({
+          id: 'hello-world-1',
+          is: HelloWorld
+        })
+      })
+    })
+  },
+
+  methods: {
+    buttonClicked () {
+      const id = `new-widget-${Math.random()}`
+      const widget = {
+        id,
+        is: HelloWorld
+      }
+      this.$refs.lumino.addWidget(widget)
+    }
   }
 }
 </script>
