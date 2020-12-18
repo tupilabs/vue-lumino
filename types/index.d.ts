00b46a9b71eed1dfa0e0c15621fc0b12d57ba465
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-12-19 00:02:20 +1300

    Add Lumino and LuminoWidget types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,20 @@
-declare module "vue-lumino";
+declare module "@tupilabs/vue-lumino" {
+  import { VueConstructor } from 'vue'
+  import { Message } from '@lumino/messaging/types'
+
+  const Lumino : VueConstructor
+
+  class LuminoWidget {
+    constructor(id: string, name: string, closable: boolean);
+    createNode(id: string): HTMLElement;
+    onActivateRequest(msg: Message);
+    onCloseRequest(msg: Message);
+    _getEventDetails();
+  }
+
+  export default Lumino;
+  export {
+    Lumino,
+    LuminoWidget
+  }
+}
