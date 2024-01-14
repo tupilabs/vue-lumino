2d092bed03046a3fd8386a39c83c871e19af2468
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2024-01-14 02:12:51 +0100

    Migrating to newer Cypress using their migration tool

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -17,10 +17,10 @@
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config)
 
-  on(
-    'file:preprocessor',
-    require('@cypress/code-coverage/use-browserify-istanbul')
-  )
+  // on(
+  //   'file:preprocessor',
+  //   require('@cypress/code-coverage/use-browserify-istanbul')
+  // )
 
   // important - return config because code coverage plugin
   // modifies environment variables there
